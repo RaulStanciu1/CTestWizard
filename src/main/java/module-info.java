1126ca3695f6481c3550bab04b2878f3a6e30b0a
RaulStanciu02 <raul.stanciu02@e-uvt.ro>
2023-10-17 21:42:37 +0300

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.ctestwizard {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.ctestwizard to javafx.fxml;
+    exports com.ctestwizard;
+    exports com.ctestwizard.controller;
+    opens com.ctestwizard.controller to javafx.fxml;
+}
